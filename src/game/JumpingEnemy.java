44391f62a7b3cd34aea1d4537ff2eefbcca130c2
Max <maximomacchi@gmail.com>
2019-01-23 10:43:23 -0800

    Time variable to track frequency of jumps initialized in JumpingEnemy

--- src/game/JumpingEnemy.java
+++ src/game/JumpingEnemy.java
@@ -1,15 +1,19 @@
 package game;
 
 public class JumpingEnemy extends Entity {
+	
+	private int timerNum;
 
 	public JumpingEnemy(String imgName, int startX, int startY, int imagesInWalk, int imagesInIdle) {
 		super(imgName, startX, startY, 64, 64, imagesInWalk, imagesInIdle);
+		timerNum = 0;
 	}
 	
 	/* This routine will determine how a jumping enemy will move. The enemy will jump every couple
 	 * of seconds and stay in the same place  */
 	public void walkMovement() {
 //		Add time to jump timer
+		timerNum++;
 //		If specific number of seconds has passed:
 //			Make enemy jump
 //			Apply physics to enemy so that gravity kicks in and brings the enemy down after they've jumped
